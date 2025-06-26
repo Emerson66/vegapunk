634ddb6a2f54bebaf79aedff058c6ea6aad5718c
Emerson.reis <emerson.costa@segod.rr.gov.br>
2025-06-26 17:10:55 -0400

    Projeto criado

---
+++ src/main/java/br/com/buritiscript/vegapunk/VegapunkApplication.java
@@ -0,0 +1,13 @@
+package br.com.buritiscript.vegapunk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegapunkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VegapunkApplication.class, args);
+	}
+
+}
